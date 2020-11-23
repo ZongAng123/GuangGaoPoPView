8c68334e4ccd9a2e2db9748d7d1c74b135421d31
zongang12 <zongang15@yahoo.com>
2020-11-23 14:48:08 +0800

    Initial Commit

---
+++ GuangGaoTanChuang/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  GuangGaoTanChuang
+//
+//  Created by ZA on 2020/11/23.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ GuangGaoTanChuang/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  GuangGaoTanChuang
+//
+//  Created by ZA on 2020/11/23.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
